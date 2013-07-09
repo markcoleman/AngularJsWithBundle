72dee89f8479aa5ca33eb278239c940de107766b
Mark Coleman <markcoleman@gmail.com>
2013-07-08 21:47:08 -0400

    Example with BundleConfig.cs and AngularJs

---
+++ AngularJsWithBundle/js/app.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../Scripts/_references.js" />
+var bundleapp = angular.module("bundleapp", []);
+
+bundleapp.factory("MyService", function () {
+    return {
+        message : "Welcome!"
+    };
+});
+
+bundleapp.controller("AngularCtrl", ["$scope", "MyService", function ($scope, MyService) {
+    $scope.message = MyService.message + " from AngularCtrl";
+}]);
+
+function MyCtrl($scope, MyService) {
+    $scope.message = MyService.message + " from MyCtrl";
+};
+
+MyCtrl.$inject = ["$scope", "MyService"];
